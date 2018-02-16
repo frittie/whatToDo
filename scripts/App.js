16b078dc9e78b89260a1642756f2cb275a7c4d39
Frida Svensson <frida.svensson@telia.com>
2018-02-16 14:49:38 +0100

    added project

---
+++ scripts/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = { 
+    activities : [],
+    activity : ""
+  }
+
+  componentDidMount() {  
+    var url = './scripts/activities/activities.json';
+  
+    fetch(url)
+    .then(response => response.json())
+    .then(responseJson => {
+      this.setState({ activities: responseJson});
+      //console.log(this.state.activities);  
+    });  
+  }
+
+  getRandomactivity = () => {
+    var getAllactivities = this.state.activities;
+    this.setState({ 
+      activity : getAllactivities[Math.floor(Math.random()*getAllactivities.length)]
+    });
+
+    console.log(this.state.activity);
+  }
+
+  render() {
+    if(this.state === null) return null;  
+
+    return (
+      <div className="wrapper">
+        <div className="content">
+          <button onClick={this.getRandomactivity} >Vad fan ska jag hitta på?</button>
+          {/* {this.state.activities.map(activity => (
+            <div key={activity.activity}>{activity.activity}</div>
+          ))} */}
+
+          <p>{this.state.activity.activity}</p>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+
